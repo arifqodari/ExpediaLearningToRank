72cfac41f0585634e3381a4dbd6041c2ed047ced
Arif Qodari <arif.qodari@gmail.com>
2015-05-01 14:59:05 +0200

    Added simple preprocessing

--- src/main.py
+++ src/main.py
@@ -1,20 +1,83 @@
 import numpy as np
 import pandas as pd
+import cPickle as pickle
 
-from data import *
+from setting import *
+from sklearn.ensemble import *
 
 
-def read_data():
+def read_data(filename):
     """
-    we don't have enough memoery to load a big file
+    we don't have enough memory to load a big file
     so we need to read it in chunks
     """
 
-    return pd.read_csv(TEST_DATA, dtype=object, chunksize=30)
+    size_in_chunk = 30
+    return pd.read_csv(filename, dtype=object, chunksize=size_in_chunk)
+
+def read_training_data():
+    """
+    read training data
+    """
+
+    return read_data(TRAIN_DATA)
+
+def read_test_data():
+    """
+    read test data
+    """
+
+    return read_data(TEST_DATA)
+
+def preprocessing(df):
+    """
+    preprocessing data
+    """
+
+    # replace missing values with -1
+    df.fillna(-1, inplace=True)
+
+    # attribute selection
+    attributes = list(df.columns)
+    attributes.remove('date_time')
+    attributes.remove('booking_bool')
+
+    # create data and target matrix
+    X = df[attributes].values
+    y = df['booking_bool'].values
+
+    return X, y
+
+def save_model(clf):
+    """
+    save classifier to file
+    """
+
+    output = open(MODEL_FILENAME, 'w')
+    pickle.dump(clf, output)
+
+def load_model():
+    """
+    load classifier from file
+    """
+
+    inp = open(MODEL_FILENAME, 'r')
+    return pickle.load(inp)
 
 
 if __name__ == "__main__":
-    reader = read_data()
+    train_reader = read_training_data()
+    test_reader = read_test_data()
 
-    for chunk in reader:
-        print chunk
+    df_train = train_reader.get_chunk(1000)
+    df_test = test_reader.get_chunk(1000)
+
+    X_train, y_train = preprocessing(df_train)
+
+    clf = RandomForestClassifier(n_estimators=50,
+            verbose=2,
+            n_jobs=2,
+            min_samples_split=10,
+            random_state=1)
+    #clf.fit(X_train, y_train)
+
--- src/setting.py
+++ src/setting.py
@@ -4,3 +4,6 @@ folder = '../data/'
 # specify filename for data train and test
 TRAIN_DATA = folder + 'training_set_VU_DM_2014.csv'
 TEST_DATA = folder + 'test_set_VU_DM_2014.csv'
+
+# specify model file name
+MODEL_FILENAME = 'model.pickle'
