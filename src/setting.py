7596c2603a74cb50870541c56b54075848016ea0
cicocn <cncico@gmail.com>
2015-05-06 00:05:22 +0200

    Add KAGGLE_TRAIN_DATA

--- src/setting.py
+++ src/setting.py
@@ -4,6 +4,7 @@ folder = '../data/'
 # specify filename for data train and test
 TRAIN_DATA = folder + 'training_set_VU_DM_2014.csv'
 TEST_DATA = folder + 'test_set_VU_DM_2014.csv'
+KAGGLE_TRAIN_DATA = folder + 'processed_training_data.csv'
 
 # specify model file name
 MODEL_FILENAME = 'model.pickle'
