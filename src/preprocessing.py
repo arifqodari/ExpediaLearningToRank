094da3054d68e2fb389608c984856a90787cb470
cicocn <cncico@gmail.com>
2015-05-08 13:19:36 +0200

    Add two processing schemes' function: one is without categorical features; one is with.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,14 +5,15 @@ import cPickle as pickle
 from setting import *
 from data_reader import *
 
-def preprocessing(df,type = 1):
+# Scheme 1: Remove all categorical attributes
+def preprocessing_1(df,type = 1):
     """
-    preprocessing data
+    Preprocessing data
     Here we only drop features and transform NULL values
     After preprocessing:
         training data (type = 1): contains only training and target
         test data (type  = 0): contains only training
-    No return values
+    No return values since all operation is done in place
     """
 
     # treatment for missing values
@@ -24,7 +25,7 @@ def preprocessing(df,type = 1):
     # Replace NULL with -10 in place
     df.visitor_hist_starrating.fillna(-10,inplace = True)
 
-    df.visitor_hist_adr_usd.fillna(-10,inplace = True)
+    #df.visitor_hist_adr_usd.fillna(-10,inplace = True)
 
     df.prop_review_score.fillna(-10, inplace = True)
 
@@ -32,15 +33,6 @@ def preprocessing(df,type = 1):
     df.srch_query_affinity_score.fillna(-350, inplace = True)
 
     df.prop_location_score2.fillna(0, inplace = True)
-
-    # Replace NULL of competitiors with 0 in place
-    # for i in range(1,9):
-    #     rate = 'comp' + str(i) + '_rate'
-    #     inv = 'comp' + str(i) + '_inv'
-    #     diff = 'comp' + str(i) + '_rate_percent_diff'
-    #     df[rate].fillna(0,inplace = True)
-    #     df[inv].fillna(0,inplace = True)
-    #     df[diff].fillna(0,inplace = True)
 
     # Remove all categorical attribute
     to_delete = [
@@ -60,22 +52,72 @@ def preprocessing(df,type = 1):
         diff = 'comp' + str(i) + '_rate_percent_diff'
         to_delete.extend([rate,inv,diff])
 
+    #This goes for the training data
     if type == 1:
         to_delete.extend(['click_bool','gross_bookings_usd'])
 
     df.drop(to_delete, axis = 1, inplace = True)
     print "The preprocessing task is done."
-    # attributes = list(df.columns)
-    # attributes.remove('srch_id')
-    # attributes.remove('date_time')
-    # attributes.remove('position')
-    # attributes.remove('click_bool')
-    # attributes.remove('gross_booking_usd')
-    # attributes.remove('booking_bool')
 
-    # create features matrix and target array
-    # X = df[attributes].values
-    # y = df['booking_bool'].values
+
+# Scheme 2: Keep all categorical attributes 
+# (except srch_id and data_time)
+def preprocessing_2(df,type = 1):
+    """
+    preprocessing data
+    Here we only drop features and transform NULL values
+    After preprocessing:
+        training data (type = 1): contains only training and target
+        test data (type  = 0): contains only training
+    No return values since all operation is done in place
+    """
+
+    # Remove srch_id and date_time
+    df.drop(['srch_id','date_time'],axis = 1, inplace = True)
+
+    # treatment for missing values
+    df.orig_destination_distance.fillna(-10,inplace = True)
+
+    # Replace NULL with -10 in place
+    df.visitor_hist_starrating.fillna(-10,inplace = True)
+
+    df.visitor_hist_adr_usd.fillna(-10,inplace = True)
+
+    df.prop_review_score.fillna(-10, inplace = True)
+
+    # Replace a value less than the minimum of training + test data
+    df.srch_query_affinity_score.fillna(-350, inplace = True)
+
+    df.prop_location_score2.fillna(0, inplace = True)
+
+    #Replace NULL of competitiors with 0 in place
+    for i in range(1,9):
+        rate = 'comp' + str(i) + '_rate'
+        inv = 'comp' + str(i) + '_inv'
+        diff = 'comp' + str(i) + '_rate_percent_diff'
+        df[rate].fillna(0,inplace = True)
+        df[inv].fillna(0,inplace = True)
+        df[diff].fillna(0,inplace = True)
+
+    #This only goes for training data
+    if type == 1:
+        df.drop(['click_bool','gross_bookings_usd'], axis = 1, inplace = True)
+
+    print "The preprocessing task is done."
+
+# Add average numeric attributes to the training and test data
+# Average is based on training and test data at the same time
+# 
+
+
+
+
+
+
+
+
+
+
 
 
 def simple_preprocessing(df):
