43503da753a6346e94096001d90816b154123266
Arif Qodari <arif.qodari@gmail.com>
2015-05-08 10:00:15 +0200

    Simple pre-processing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -78,3 +78,44 @@ def preprocessing(df,type = 1):
     # y = df['booking_bool'].values
 
 
+def simple_preprocessing(df):
+    """
+    simple pre-processing
+    """
+
+    # treatment for missing values
+    df.orig_destination_distance.fillna(-10,inplace = True)
+
+    df.visitor_hist_starrating.fillna(-10,inplace = True)
+
+    df.visitor_hist_adr_usd.fillna(-10,inplace = True)
+
+    df.prop_review_score.fillna(-10, inplace = True)
+
+    # Replace a value less than the minimum of training + test data
+    df.srch_query_affinity_score.fillna(-350, inplace = True)
+
+    df.prop_location_score2.fillna(0, inplace = True)
+
+    # Replace NULL of competitiors with 0 in place
+    for i in range(1,9):
+        rate = 'comp' + str(i) + '_rate'
+        inv = 'comp' + str(i) + '_inv'
+        diff = 'comp' + str(i) + '_rate_percent_diff'
+        df[rate].fillna(0,inplace = True)
+        df[inv].fillna(0,inplace = True)
+        df[diff].fillna(0,inplace = True)
+
+    attributes = list(df.columns)
+    attributes.remove('srch_id')
+    attributes.remove('date_time')
+    attributes.remove('position')
+    attributes.remove('click_bool')
+    attributes.remove('gross_bookings_usd')
+    attributes.remove('booking_bool')
+
+    # create features matrix and target array
+    X = df[attributes].astype(float).values
+    y = df['booking_bool'].astype(int).values
+
+    return X, y
